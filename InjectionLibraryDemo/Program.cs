d06cbfa69a6367406b3b0dc437f711ca8122e9f5
Constantine <const-fi@hotmail.com>
2017-12-27 16:11:29 +0300

    Add project files.

---
+++ InjectionLibraryDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace InjectionLibraryDemo
+{
+    class Program
+    {
+        [DllImport("InjectedLib.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern int magic();
+
+        static void Main()
+        {
+            var a = 10;
+            if (!(a <= 0))
+            {
+            }
+
+            Console.WriteLine(magic());
+        }
+    }
+}
